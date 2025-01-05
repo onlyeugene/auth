38448a6bbc9f23be336f84d3ffc2b3220772aeee
onlyeugene <eugeneokogun@gmail.com>
2025-01-05 20:35:04 +0100

    small changes

--- components/auth/back-button.tsx
+++ components/auth/back-button.tsx
@@ -12,7 +12,7 @@ export const BackButton = ({ label, href }: BackButtonProps) => {
   return (
     <Button
     variant='link'
-    className="font-normal w-ful"
+    className="font-normal w-full"
     size='sm'
     asChild
     >
