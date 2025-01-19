a211e69e820a3fe3207e7ed2bed9371a76b407b0
onlyeugene <eugeneokogun@gmail.com>
2025-01-19 09:07:00 +0100

    feat: updated the contacts view and edit design

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,8 @@ export default function Home() {
           Auth
         </h1>
         <p className={cn(`text-white text-lg`, font.className)}>
-          A simple authentication service
+
+
         </p>
         <div>
           <LoginButton mode="modal" asChild>
