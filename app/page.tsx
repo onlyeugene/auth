54eb9b396bf0486f5e7289bfd2726a077e92566b
onlyeugene <eugeneokogun@gmail.com>
2025-01-20 10:27:11 +0100

    updated codes

--- app/page.tsx
+++ app/page.tsx
@@ -20,9 +20,7 @@ export default function Home() {
         >
           Auth
         </h1>
-        <p className={cn(`text-white text-lg`, font.className)}>
-          A simple authentication service
-        </p>
+        <p className={cn(`text-white text-lg`, font.className)}></p>
         <div>
           <LoginButton mode="modal" asChild>
             <Button variant="secondary" size="lg">
